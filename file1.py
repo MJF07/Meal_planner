4c7ebfefae7b1527c4925b39292c2a0baad187c7
MJF07 <maxfrankel@stu.postoakschool.org>
2025-09-10 11:26:45 -0500

    this is my first commit

--- file1.py
+++ file1.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return"<hi>This Just In:You're an idiot</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)

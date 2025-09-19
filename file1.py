591e576498c1ad37cce1bf6d56d5fe9386a005ae
MJF07 <maxfrankel@stu.postoakschool.org>
2025-09-19 12:59:49 -0500

    this is my second commit

--- file1.py
+++ file1.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return"<hi>This Just In:You're an idiot</h1>"
+def index():
+    return render_template("index.html",message="Welcome. Please enter your username and password.")
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
 
+    print("Username", username)
+    print("Password", password)
+
+    if username == "walter" and password == "mirror":
+        return "<h2>Access granted</h2>"
+    else:
+        return "<h2>Access denied</h2>", 401
 if __name__ == "__main__":
     app.run(debug=True)

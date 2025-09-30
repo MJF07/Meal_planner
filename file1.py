0bacce41e80c40791fdb65921b61701fe69e8618
MJF07 <maxfrankel@stu.postoakschool.org>
2025-09-30 18:47:49 -0500

    this is my second commit

--- file1.py
+++ file1.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template,request
+import csv
+from pathlib import Path
 
 app = Flask(__name__)
+USERS_CSV = Path(__file__).parent / "users.csv"
 
 @app.route("/")
 def index():
@@ -12,10 +15,13 @@ def login():
 
     print("Username", username)
     print("Password", password)
-
-    if username == "walter" and password == "mirror":
-        return "<h2>Access granted</h2>"
-    else:
-        return "<h2>Access denied</h2>", 401
+    with open(USERS_CSV, newline='', encoding="utf-8") as list:
+        reader = csv.DictReader(list)
+        for row in reader:
+            if row['username'] == username and row['password'] == password:
+                print("ok")
+                return "<h2>Access granted</h2>"
+            else:
+                return "<h2>Access denied</h2>", 401
 if __name__ == "__main__":
     app.run(debug=True)
